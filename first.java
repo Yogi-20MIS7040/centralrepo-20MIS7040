b8463c7040d465bcbe3806f903d9f1b78c1f2ebe
Yogi-20MIS7040 <76085873+Yogi-20MIS7040@users.noreply.github.com>
2021-03-13 12:15:55 +0530

    Revert "add feature 1 and feature 1 AR"

--- first.java
+++ first.java
@@ -1,8 +1,5 @@
-iclass Simple{  
+class Simple{  
     public static void main(String args[]){  
      System.out.println("  yogi ");  
-    }  int y
-    int p
-	    int s 
-	    System.out.println("ADDING FEATURE 1 TO APRIL RELEASE")
+    }  
 }  
